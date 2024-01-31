f0058100554e913f129dbde5481ceb2c04295c2a
Valerii_Panov <val250640@mail.ru>
2024-02-01 03:06:43 +1000

    hw02

---
+++ Homework02/script.js
@@ -0,0 +1,11 @@
+// Task №1
+let num1 = Number(prompt("Num1 = "));
+let num2 = Number(prompt("Num2 = "));
+
+console.log(num1 <= 1 && num2 >= 3);
+
+
+// Task №2
+let test = true;
+
+console.log(test === true ? '+++' : '---');
